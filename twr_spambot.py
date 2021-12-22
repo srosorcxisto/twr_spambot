8c36746e5d9778e4159cabc95277914592507fd3
wkelly19 <wkelly19@jhmi.edu>
2021-12-22 10:20:54 -0600

    Cleaned up header

--- twr_spambot.py
+++ twr_spambot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-""" Short description of this Python module.
+"""
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
 Foundation, either version 3 of the License, or (at your option) any later
